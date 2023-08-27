e6e0dff563e08c0a3c903372d190a666e9640638
janvikathiriya <129316889+janvikathiriya@users.noreply.github.com>
2023-08-27 15:02:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import Weather from "./components/weather";
+
+
+// function App() {
+//   return(
+//     <>
+//     <div className="container">
+//       <Weather/>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+import WeatherForecast from "./components/WeatherForecast";
+
+function App() {
+  return (
+    
+      <div className="container"><WeatherForecast/></div>
+    
+  );
+}
+
+export default App;
